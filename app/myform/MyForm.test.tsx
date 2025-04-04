2115c2dcfa63357a060ac4d58e9f5e844b6500a2
Leo Danielsson <leo.danielsson@ub.uu.se>
2025-04-04 20:32:39 +0200

    make sure test fails when error boundary rendersgp

--- app/myform/MyForm.test.tsx
+++ app/myform/MyForm.test.tsx
@@ -1,14 +1,15 @@
-import { render } from "@testing-library/react";
-import { describe, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
 import MyForm from "./MyForm";
 import { createRoutesStub } from "react-router";
 
 describe('MyForm', () => {
     it('renders', () => {
-        
         const RoutesStub = createRoutesStub([
             { path: '/', action: () => {}, Component: MyForm}
         ])
         render(<RoutesStub />)
+        
+        screen.getByLabelText('Name:');
     })
 })
